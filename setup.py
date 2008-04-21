6596d249f2ade03bdaf8f360bbba54cc620c209f
Andreas Kloeckner <inform@tiker.net>
2008-04-21 17:50:22 -0400

    Setup and Metadata fixes.

--- setup.py
+++ setup.py
@@ -34,11 +34,42 @@ def main():
     execfile("src/python/__init__.py", conf)
     setup(name="PyMetis",
           version="0.91",
-          description="A wrapper around Metis",
+          description="A Graph Partitioning Package",
+          long_description="""
+          PyMetis is a Python wrapper for the
+          `Metis <http://glaros.dtc.umn.edu/gkhome/views/metis>`_ graph
+          partititioning software by George Karypis, Vipin Kumar and others. It
+          includes version 5.0pre2 of Metis and wraps it using the 
+          `Boost Python <http://www.boost.org/libs/python/doc/>`_ wrapper generator
+          library. So far, it only wraps the most basic graph partitioning
+          functionality (which is enough for my current use), but extending it
+          in case you need more should be quite straightforward. Using PyMetis
+          to partition your meshes is really easy--essentially all you need to
+          pass into PyMetis is an adjacency list for the graph and the number
+          of parts you would like.
+          """,
           author="Andreas Kloeckner",
           author_email="inform@tiker.net",
-          license = "BSD for the wrapper/",
-          url="http:///software/pymetis",
+          license = "wrapper: MIT/code: Free for research and non-commercial use",
+          url="http://mathema.tician.de/software/pymetis",
+          classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Other Audience',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'License :: Free for non-commercial use',
+            'Natural Language :: English',
+            'Programming Language :: C',
+            'Programming Language :: C++',
+            'Programming Language :: Python',
+            'Topic :: Multimedia :: Graphics :: 3D Modeling',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Scientific/Engineering :: Visualization',
+            'Topic :: Software Development :: Libraries',
+            ],
+
           packages = [ "pymetis" ],
           package_dir={"pymetis": "src/python"},
           ext_modules = [
