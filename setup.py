6e8c83d5c24aacb843ed0d1db23706734120377f
Andreas Kloeckner <inform@tiker.net>
2019-01-03 10:44:16 -0500

    Use pybind11 setup widgetry from aksetup

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import setuptools
-from setuptools.command.build_ext import build_ext
-
 
 def get_config_schema():
     from aksetup_helper import ConfigSchema, StringListOption
@@ -14,87 +10,13 @@ def get_config_schema():
         ])
 
 
-# {{{ boilerplate from https://github.com/pybind/python_example/blob/2ed5a68759cd6ff5d2e5992a91f08616ef457b5c/setup.py  # noqa
-
-class get_pybind_include(object):  # noqa: N801
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False) as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        fname = f.name
-    try:
-        compiler.compile([fname], extra_postargs=[flagname])
-    except setuptools.distutils.errors.CompileError:
-        return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct in ['unix', 'mingw32']:
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = ext.extra_compile_args + opts
-        build_ext.build_extensions(self)
-
-# }}}
-
-
 def main():
     import glob
     from setuptools import find_packages
     from aksetup_helper import (
             check_pybind11,
-            hack_distutils, get_config, setup, Extension)
+            hack_distutils, get_config, setup, Extension,
+            get_pybind_include, PybindBuildExtCommand)
 
     check_pybind11()
 
@@ -102,9 +24,9 @@ def main():
     conf = get_config(get_config_schema(),
             warn_about_no_config=False)
 
-    EXTRA_DEFINES = {}
+    extra_defines = {}
 
-    EXTRA_DEFINES["HAVE_MREMAP"] = 0  # mremap() buggy on amd64?
+    extra_defines["HAVE_MREMAP"] = 0  # mremap() buggy on amd64?
 
     ver_filename = "pymetis/version.py"
     version_file = open(ver_filename)
@@ -157,22 +79,22 @@ def main():
           ext_modules=[
               Extension(
                   "pymetis._internal",
-                  ["src/wrapper/wrapper.cpp"] +
-                  glob.glob("src/metis/GKlib/*.c") +
-                  glob.glob("src/metis/*.c") +
-                  glob.glob("src/metis/libmetis/*.c"),
-                  define_macros=list(EXTRA_DEFINES.items()),
-                  include_dirs=["src/metis/GKlib"] +
-                  ["src/metis/include"] +
-                  ["src/metis/libmetis"] +
-                  [
+                  ["src/wrapper/wrapper.cpp"]
+                  + glob.glob("src/metis/GKlib/*.c")
+                  + glob.glob("src/metis/*.c")
+                  + glob.glob("src/metis/libmetis/*.c"),
+                  define_macros=list(extra_defines.items()),
+                  include_dirs=["src/metis/GKlib"]
+                  + ["src/metis/include"]
+                  + ["src/metis/libmetis"]
+                  + [
                         get_pybind_include(),
                         get_pybind_include(user=True)
                         ],
                   extra_compile_args=conf["CXXFLAGS"],
                   ),
               ],
-            cmdclass={'build_ext': BuildExt},
+            cmdclass={'build_ext': PybindBuildExtCommand},
             zip_safe=False)
 
 
