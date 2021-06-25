bda5082563361141dc03fcc606aaba494ad40fa6
Andreas Kloeckner <inform@tiker.net>
2021-06-25 13:55:11 -0500

    Use downloaded shared sphinxconfig

--- doc/conf.py
+++ doc/conf.py
@@ -1,17 +1,10 @@
-# -- Path setup --------------------------------------------------------------
+from urllib.request import urlopen
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+_conf_url = \
+        "https://raw.githubusercontent.com/inducer/sphinxconfig/main/sphinxconfig.py"
+with urlopen(_conf_url) as _inf:
+    exec(compile(_inf.read(), _conf_url, "exec"), globals())
 
-
-# -- Project information -----------------------------------------------------
-
-project = "pymetis"
 copyright = "2013-2021, Andreas Kloeckner"
 author = "Andreas Kloeckner"
 
@@ -28,34 +21,3 @@ def get_version():
 
 
 version = get_version()
-
-# -- General configuration ---------------------------------------------------
-
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.intersphinx",
-    "sphinx_copybutton",
-]
-
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
