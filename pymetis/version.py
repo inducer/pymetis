11eafbf52bfcd06ee0ebe65709d8c9eb7bd40166
Andreas Kloeckner <inform@tiker.net>
2025-07-17 14:22:22 -0500

    version: fix _parse_version, add __all__

--- pymetis/version.py
+++ pymetis/version.py
@@ -4,7 +4,7 @@ from importlib import metadata
 def _parse_version(version: str) -> tuple[tuple[int, ...], str]:
     import re
 
-    m = re.match(r"^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+    m = re.match(r"^([0-9.]+)([a-z0-9]*?)$", version)
     assert m is not None
 
     return tuple(int(nr) for nr in m.group(1).split(".")), m.group(2)
@@ -16,3 +16,11 @@ VERSION, VERSION_STATUS = _parse_version(VERSION_TEXT)
 # deprecated
 version = VERSION_TEXT
 version_tuple = VERSION
+
+__all__ = [
+    "VERSION",
+    "VERSION_STATUS",
+    "VERSION_TEXT",
+    "version",
+    "version_tuple"
+]
